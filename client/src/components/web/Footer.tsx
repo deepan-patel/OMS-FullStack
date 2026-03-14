b26ecc1ebe5442da27be23596df656d23a7dfc8e
Deepan Patel <55546591+deepan-patel@users.noreply.github.com>
2026-03-14 15:21:02 -0400

    configured shadcn theme
    -- configured shadcn theme

--- client/src/components/web/Footer.tsx
+++ client/src/components/web/Footer.tsx
@@ -1,7 +1,32 @@
+
+import { footerData } from "@/app/data/data-config";
+import Link from "next/link";
+
 export default function Footer() {
     return (
-        <footer>
-            <p>Footer</p>
-        </footer>
+        <div className="mt-16 flex flex-col md:flex-row items-center md:item-start bg-accent p-5 rounded-sm">
+            <div className="flex-col gap-4 items-center md:items-start">
+                <p className="text-sm text-gray-500">Order Management System</p>
+                <p className="text-sm text-gray-500">© 2026 OMS. All rights reserved.</p>
+            </div>
+            <div className="flex-row justify-between">
+                {
+                    footerData.map((item, index) => (
+                        <div key={index} className="flex-col gap-4 items-center md:items-start">
+                            <p className="text-sm text-gray-500">{item.title}</p>
+                            <div className="flex flex-col gap-4">
+                                {
+                                    item.links.map((link, index) => (
+                                        <Link key={index} href={link.href} className="text-sm text-gray-500">{link.label}</Link>
+                                    ))
+                                }
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+
+        </div>
+
     )
 }
--- client/src/components/web/Navbar.tsx
+++ client/src/components/web/Navbar.tsx
@@ -1,18 +1,23 @@
 import Link from "next/link";
-import { ShoppingBag } from "lucide-react";
+import { Bell, Home, ShoppingBag, ShoppingCart } from "lucide-react";
 import { buttonVariants } from "../ui/button";
+import SearchBar from "./SearchBar";
 
 export default function NavBar() {
     return (
         <nav className="w-full flex justify-between items-center bg-accent p-5 mb-5 rounded-xl">
             <div className="flex items-center gap-2">
-                <ShoppingBag className="w-6 h-6 md:w-9 md:h-9 hover:text-primary" />
-                <span className="text-bold tex-xl md:text-2xl tracking-wider hover:text-primary">OMS FullStack Project</span>
+                <ShoppingBag className="w-6 h-6 md:w-7 md:h-7" />
+                <span className="text-bold tex-xl md:text-2xl tracking-wider ">OMS</span>
             </div>
 
 
-            <div>
-                <Link className={buttonVariants({ variant: "default" })} href="/sign-in" >Sign In</Link>
+            <div className="flex items-center gap-6">
+                <SearchBar />
+                <Link href="/"><Home className="hover:text-primary text-gray-500" /></Link>
+                <Bell className="hover:text-primary w-9 h-9 text-gray-500" />
+                <ShoppingCart className="hover:text-primary w-9 h-9 text-gray-500" />
+                <Link className={buttonVariants({ variant: "default", className: "hover:text-primary" })} href="/sign-in" >Sign In</Link>
             </div>
 
         </nav>
